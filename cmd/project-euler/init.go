e9bdf3d17ca3ea9fc4a7a530cc94f12b9c19191d
adarshmelethil <adarshmelethil@gmail.com>
2019-07-19 03:49:02 -0600

    Project euler #2

--- cmd/project-euler/init.go
+++ cmd/project-euler/init.go
@@ -1,11 +1,14 @@
 package euler
 
 import (
-	"github.com/spf13/cobra"
+  "github.com/spf13/cobra"
 )
 
+// AddSubCommands Adds this project's commands as the subcommad to the parent
 func AddSubCommands(parent *cobra.Command) {
-	setOneCmdFlags()
-	parent.AddCommand(oneCmd)
-}
+  setOneCmdFlags()
+  parent.AddCommand(oneCmd)
 
+  setTwoCmdFlags()
+  parent.AddCommand(twoCmd)
+}
